ddb62182975a1716da1d24328deecb5b7a27ccb3
Olivier Lance <bestiol@gmail.com>
2012-05-29 02:16:55 +0200

    Initial Commit

---
+++ Cocolizer/CLDocument.h
@@ -0,0 +1,13 @@
+//
+//  CLDocument.h
+//  Cocolizer
+//
+//  Created by lacie on 28/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface CLDocument : NSPersistentDocument
+
+@end
---
+++ Cocolizer/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Cocolizer
+//
+//  Created by lacie on 28/05/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
